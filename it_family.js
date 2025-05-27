a7ba14189caa25255eb393c04a8916dbb748c54b
itfamily316 <ds4321940@gmail.com>
2025-05-27 10:36:42 +0200

    Delete it_family.js

--- it_family.js
+++
@@ -1,7 +0,0 @@
-const texts = ["سحر التكنولوجيا ", "عولمة لا محدودة", "إتصالات آمنة", "هندسة النظم"];
-  let index = 0;
-
-  setInterval(() => {
-    index = (index + 1) % texts.length;
-    document.getElementById("changing").textContent = texts[index];
-  }, 2000); // Change text every 2 seconds
