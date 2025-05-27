38c391fbc95607762364f632accb8d21d8d18dbe
itfamily316 <ds4321940@gmail.com>
2025-05-27 10:03:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const texts = ["سحر التكنولوجيا ", "عولمة لا محدودة", "إتصالات آمنة", "هندسة النظم"];
+const texts = ["أنظمة آمنة","تبادل معلومات","إكتسب خبرات","إكتفاء ذاتي","تقنية متجددة"]
   let index = 0;
 
   setInterval(() => {
     index = (index + 1) % texts.length;
     document.getElementById("changing-text").textContent = texts[index];
-  }, 4000); // Change text every 2 seconds
+  }, 2000); // Change text every 2 seconds
+function home() {
+  document.getElementById("main-page").style.display = "block ";  
+document.getElementById("news-page").style.display ="none";    
+}
+function news() {
+  document.getElementById("main-page").style.display = "none";  
+document.getElementById("news-page").style.display ="block";
+}
+function media() {
+document.getElementById("overlay").style.display="flex";  
+}
+function confirmYes() {
+document.getElementById("overlay").style.display="none";    
+}
